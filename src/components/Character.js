09044bed385cf3273192275af94f7b6e9e6c3426
Paul Rail <paul.a.rail@gmail.com>
2019-05-26 23:24:22 -0400

    useEffect to replace componentDidMount + componentDidUpdate + componentWillUnmount

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,30 +1,80 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Summary from './Summary';
 
-class Character extends Component {
-  state = { loadedCharacter: {}, isLoading: false };
+// class Character extends Component {
+const Character = props => {
+  // state = { loadedCharacter: {}, isLoading: false };
+  const [loadedCharacter, setLoadedCharacter] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('shouldComponentUpdate');
-    return (
-      nextProps.selectedChar !== this.props.selectedChar ||
-      nextState.loadedCharacter.id !== this.state.loadedCharacter.id ||
-      nextState.isLoading !== this.state.isLoading
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log('shouldComponentUpdate');
+  //   return (
+  //     nextProps.selectedChar !== this.props.selectedChar ||
+  //     nextState.loadedCharacter.id !== this.state.loadedCharacter.id ||
+  //     nextState.isLoading !== this.state.isLoading
+  //   );
+  // }
+
+  // brought up from below:
+  const fetchData = () => {
+    console.log(
+      'Sending Http request for new character with id ' +
+        props.selectedChar
     );
-  }
+    setIsLoading(true);
+    fetch('https://swapi.co/api/people/' + props.selectedChar)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Could not fetch person!');
+        }
+        return response.json();
+      })
+      .then(charData => {
+        const loadedCharacter = {
+          id: props.selectedChar,
+          name: charData.name,
+          height: charData.height,
+          colors: {
+            hair: charData.hair_color,
+            skin: charData.skin_color
+          },
+          gender: charData.gender,
+          movieCount: charData.films.length
+        };
+        setIsLoading(false);
+        setLoadedCharacter(loadedCharacter);
+      })
+      .catch(err => {
+        console.log(err);
+        setIsLoading(false);
+      });
+  };
 
-  componentDidUpdate(prevProps) {
-    console.log('Component did update');
-    if (prevProps.selectedChar !== this.props.selectedChar) {
-      this.fetchData();
-    }
-  }
+  // componentDidUpdate(prevProps) {
+  //   console.log('Component did update');
+  //   if (prevProps.selectedChar !== this.props.selectedChar) {
+  //     this.fetchData();
+  //   }
+  // }
 
-  componentDidMount() {
-    this.fetchData();
-  }
+  // componentDidMount() {
+  //   this.fetchData();
+  // }
 
+  // for componentDidMount(), + componentDidUpdate() + componentWillUnmount():
+  useEffect(() => {    // for componentDidMount()
+    console.log("this only fires when a new character is selected");
+    fetchData();
+    return () => {   // for componentWillUnmount()
+      console.log('Cleaning up...');
+    };
+  }, [props.selectedChar]);  // for componentDidUpdate()
+  // here we are only calling this fetch data ONLY for when a new character is selected
+
+  /*
   fetchData = () => {
     console.log(
       'Sending Http request for new character with id ' +
@@ -56,30 +106,48 @@ class Character extends Component {
         console.log(err);
       });
   };
+  */
 
-  componentWillUnmount() {
-    console.log('Too soon...');
-  }
 
-  render() {
+  // useEffect can also deal with componentWillUnmount(), by returning something (a function), shown above
+
+  // componentWillUnmount() {
+  //   console.log('Too soon...');
+  // }
+
+  // render() {
     let content = <p>Loading Character...</p>;
 
-    if (!this.state.isLoading && this.state.loadedCharacter.id) {
+    // if (!this.state.isLoading && this.state.loadedCharacter.id) {
+    //   content = (
+    //     <Summary
+    //       name={this.state.loadedCharacter.name}
+    //       gender={this.state.loadedCharacter.gender}
+    //       height={this.state.loadedCharacter.height}
+    //       hairColor={this.state.loadedCharacter.colors.hair}
+    //       skinColor={this.state.loadedCharacter.colors.skin}
+    //       movieCount={this.state.loadedCharacter.movieCount}
+    //     />
+    //   );
+    if (!isLoading && loadedCharacter.id) {
       content = (
         <Summary
-          name={this.state.loadedCharacter.name}
-          gender={this.state.loadedCharacter.gender}
-          height={this.state.loadedCharacter.height}
-          hairColor={this.state.loadedCharacter.colors.hair}
-          skinColor={this.state.loadedCharacter.colors.skin}
-          movieCount={this.state.loadedCharacter.movieCount}
+          name={loadedCharacter.name}
+          gender={loadedCharacter.gender}
+          height={loadedCharacter.height}
+          hairColor={loadedCharacter.colors.hair}
+          skinColor={loadedCharacter.colors.skin}
+          movieCount={loadedCharacter.movieCount}
         />
       );
-    } else if (!this.state.isLoading && !this.state.loadedCharacter.id) {
+    // } else if (!this.state.isLoading && !this.state.loadedCharacter.id) {
+    //   content = <p>Failed to fetch character.</p>;
+    // }
+    } else if (!isLoading && !loadedCharacter.id) {
       content = <p>Failed to fetch character.</p>;
     }
     return content;
-  }
+  // }
 }
 
 export default Character;
